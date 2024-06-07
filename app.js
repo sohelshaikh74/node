f0300f5c74e51c0bf862061b6e8692b094c26b25
Sohel Shaikh <sohelshaikh30611@gmail.com>
2024-06-07 17:31:32 +0530

    second updated

--- app.js
+++ app.js
@@ -1,23 +1,14 @@
 // how to get input form user
-// const readline = require('readline')
-// const rl=readline.createInterface({
-//     input:process.stdin,
-//     output:process.stdout
-// })
-// rl.question("enter your name ",(name)=>{
-//     console.log(name);
-//     rl.close()
-// })
-
-// rl.on('close',()=>{
-//     console.log("interface close");
-//     process.exit(0)
-// })
-
-// how to read file 
-const fileSystem=require('fs')
-const content=fileSystem.readFileSync('./input.txt','utf-8')
-console.log(content);
-
-// how to write file 
-fileSystem.writeFileSync('./output.txt',content)
+const readline=require('readline')
+const rl=readline.createInterface({
+    input:process.stdin,
+    output:process.stdout
+})
+rl.question("enter your name ",(name)=>{
+    console.log(name);
+    rl.close()
+})
+rl.on('close',()=>{
+    console.log('interface closed');
+    process.exit(0)
+})
